3bf4988f69f498a6347b87c9a4684c29992507c6
Shekhar088 <shekharsth2020@gmail.com>
2021-09-05 19:25:52 +0545

    Payment invoice

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,11 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import OrderForm
 from carts.models import CartItem
 import datetime
-from .models import Order, Payment
+from .models import Order, Payment,OrderProduct
+from store.models import Product
 import json
 
 # Create your views here.
@@ -26,6 +27,37 @@ def payments(request):
     order.payment= payment
     order.is_ordered = True
     order.save()
+    #move cart items to roder product table
+
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+
+
+
+        # Reduce the quantity from stock
+        product = Product.objects.get(id=item.product_id)
+        product.stock -= item.quantity
+        product.save()
+    
+
+    #clear cart
+    CartItem.objects.filter(user=request.user).delete()
+    #send data back via json
+    data ={
+        'order_number' : order.order_number,
+        'transID': payment.payment_id,
+    }
+    return JsonResponse(data)
     return render(request, 'orders/payments.html')
 
 def place_order(request, total=0, quantity=0,):
@@ -86,52 +118,29 @@ def place_order(request, total=0, quantity=0,):
             return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
-#def place_order(request,total=0, quantity=0):
-    current_user = request.user
-    # If cart count is 0 reditect to store
 
-    cart_items = CartItem.objects.filter(user=current_user)
-    cart_count = cart_items.count()
-    if cart_count == 0:
-        return redirect('store')
+def order_complete(request):
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
 
-    grand_total =0
-    tax=0
-    for cart_item in cart_items:
-        total += (cart_item.product.price * cart_item.quantity)
-        quantity += cart_item.quantity
-    tax = (5*total)/100
-    grand_total = (tax+total)
-        
-    if request.method =="POST":
-        form = OrderForm(request.POST)
-        if form.is_valid:
-            #store billing info in order table
-            data = Order()
-            data.first_name = form.cleaned_data['first_name']
-            data.last_name = form.cleaned_data['last_name']
-            data.phone_number = form.cleaned_data['phone_number']
-            data.email = form.cleaned_data['email']
-            data.address1 = form.cleaned_data['address1']
-            data.address2 = form.cleaned_data['address2']
-            data.city = form.cleaned_data['city']
-            data.country = form.cleaned_data['country']
-            data.state = form.cleaned_data['state']
-            data.order_note = form.cleaned_data['order_note']
-            data.order_total = grand_total
-            data.tax = tax
-            data.ip = request.META.get('REMOTE_ADDR')
-            data.save()
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
 
-            #generate order number
-            yr= int(datetime.date.today().strftime('%Y'))
-            dt= int(datetime.date.today().strftime('%d'))
-            mt= int(datetime.date.today().strftime('%m'))
-            d= datetime.date(yr,mt,dt)
-            current_date = d.strftime("%Y%m%d")
-            order_number = current_date + str(data.id)
-            data.order_number = order_number
-            data.save()
-            return redirect('checkout')
-        else:
-            return redirect('checkout')
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+
+        payment = Payment.objects.get(payment_id=transID)
+
+        context = {
+            'order': order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment,
+            'subtotal': subtotal,
+        }
+        return render(request, 'orders/order_complete.html', context)
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('index')
